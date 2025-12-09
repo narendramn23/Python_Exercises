76b0ffede1bd95438f4ea82c532296f2a9d2bd1e
NARENDRA M N <narendramn21@gmail.com>
2025-12-09 21:51:00 +0530

    Milage calculator

---
+++ Intro_Set2_Q1_ODO.py
@@ -0,0 +1,24 @@
+"""
+
+
+"""
+#starting odo meter rading
+Initial_reading = int(input("Enter iniitial reading: "))
+
+#ending reading
+final_reading = int(input("Enter final reading: "))
+
+#total diatance travelled 
+distance_traveled = final_reading - Initial_reading
+
+print("distance traveled:",distance_traveled,"KM")
+
+# what is the fule consumation ?
+fuel_consume = int(input("enter fuel consumed to travel: "))
+
+#milage of my car by assuming fuel consumtion
+milage = distance_traveled / fuel_consume
+
+print("milage is:", milage,"KM")
+
+

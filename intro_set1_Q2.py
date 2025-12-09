114a21a34c3215dca040ec1f0e3f4528c35e9932
NARENDRA M N <narendramn21@gmail.com>
2025-12-09 21:32:32 +0530

    Tables with user choice
    modified S1_Q2

--- intro_set1_Q2.py
+++ intro_set1_Q2.py
@@ -1,4 +1,13 @@
 
+'''
+This program prints tables with user choice  
+
+'''
+user_choice = int(input("Please enter which tables you want : "))
+
+for multiples in range (1, 11):
+
+    answer = user_choice * multiples
 
 
-
+    print(user_choice, "X", multiples, "=" ,answer)

3f52aaab465b16de4789b4b4ef42b743150a2cd2
NARENDRA M N <narendramn21@gmail.com>
2025-12-09 10:17:34 +0530

    using divide function

--- master.py
+++ master.py
@@ -1,26 +1,22 @@
+def divide(num, din):
+    quot = num // din
+    rem = num % din
+    return quot, rem
+    
 total = 10000
 effort = 3
 
-days  = total // effort
-hrs   = total % effort
+#days = total // effort
+#rem_hrs = total % effort
+days, hrs = divide(total, effort)
 
-month = days // 30 
-days  = days % 30
+#month  = days // 30 
+#days  = days % 30
+month,days = divide(days, 30)
 
-years = month // 12
-month = month % 12
+#years = month // 12
+#month = month % 12
+years, month = divide(month, 12)
+
 
 print(years, "yrs", month, "months", days, "days", hrs, "hrs")
-
-
-#function divide(num, den)
-#   quot = num / den
-#   rem = num % den
-#   return quot, rem
-
-#total = 10000
-#effort = 3
-
-#days, hrs = divide(total, effort)
-#month, days = divide(days, 30 )
-#years, month = divide(month, 12 )

ce2dd91f64e79c0af7f7c7bff1ae8f95c66b739f
NARENDRA M N <narendramn21@gmail.com>
2025-12-09 21:23:41 +0530

    Empty file

--- intro_set1_Q2.py
+++ intro_set1_Q2.py
@@ -1,16 +1,4 @@
 
-boot flow 
-what is jump code >>>> 
 
-BIOS >>>HOB hand of block
-
-bios boot folw on all architcture 
-
-CPU >>>>
-
-PCIE 
-CXL ,power management ,sleep state .
-
-Discrete function 
 
 

b9fa43023858262155c106415b81192fcf4ed8f6
NARENDRA M N <narendramn21@gmail.com>
2026-01-02 21:27:14 +0530

    17 tables using function

---
+++ S01_Q2_function.py
@@ -0,0 +1,19 @@
+def table(a= 17):
+    for num in range(1, 11):
+         ans = num * a
+         print(a, "X",  num, "=", ans)
+
+
+
+         
+if __name__ == "__main__":
+    print("tables",__name__)         
+    table()
+
+
+
+
+
+
+
+

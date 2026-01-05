188da4046e79faf6f0959ebc3db8c96d9a531163
NARENDRA M N <narendramn21@gmail.com>
2026-01-05 22:18:22 +0530

    Prompt the user to enter a long sentence

---
+++ S7_string_3.py
@@ -0,0 +1,10 @@
+sent = "Web Browser"
+
+print("last character in the sentence: ", sent[-1:])
+print("last 5 characters in the sentence: ", sent[-5:])
+print("Characters that are present in 2nd and 5th position of the sentence", sent[1:2],"&", sent[4:5])
+#print(sent[4:5])
+print("character at center is r so printing: ", sent[4:7])
+
+
+

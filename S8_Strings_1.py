2f9407e996b85052f7f907641af5678d2c091c15
NARENDRA M N <narendramn21@gmail.com>
2026-01-07 22:47:09 +0530

    number of digits in that number

---
+++ S8_Strings_1.py
@@ -0,0 +1,25 @@
+def num(user_num):
+    if user_num.startswith("-"):
+        user_num = user_num[1:]
+
+    return len(user_num)
+
+
+
+
+user_num = input("enter number :")
+a = num(user_num)
+print("Number of digits in given number is:>>", a)
+
+    
+    
+    
+    
+    
+    
+    
+          
+
+
+
+    
